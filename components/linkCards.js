8d0678b5f21a26c8cbfd03c4492643e6c35c2282
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2022-10-28 21:58:13 +0200

    initial commit

---
+++ components/linkCards.js
@@ -0,0 +1,25 @@
+import categories from "../data/flix"
+
+export default function LinkCards() {
+  return (
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
+      {categories().map((person) => (
+        <div
+          key={person.id}
+          className="relative flex items-center space-x-3 rounded-lg border border-gray-300 bg-white px-6 py-5 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
+        >
+          <div className="flex-shrink-0">
+            <div className="h-10 w-10 rounded-full">{person.emoji}</div>
+          </div>
+          <div className="min-w-0 flex-1">
+            <a href="#" className="focus:outline-none">
+              <span className="absolute inset-0" aria-hidden="true" />
+              <p className="text-sm font-medium text-gray-900">{person.genre}</p>
+              <p className="truncate text-sm text-gray-500">({person.sub.length})</p>
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
