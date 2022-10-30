fa90aaf749de9021218981f2d3fee340ed53593e
Alejandro Close <alejandroclose@Alejandros-MacBook-Pro.local>
2022-10-31 00:28:08 +0100

    fixed bug

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import { getCategoryData } from "../lib/categories";
 
 import Navbar from "../components/navbar";
@@ -13,14 +11,14 @@ export default function Home(props) {
         <title>Nicheflix | Find your next binge</title>
         <meta
           name="description"
-          content="Find your next binge on Netflix's hidden curated categories"
+          content="Find your next binge on Netflix&apos;s hidden curated categories"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
       <div className="px-20 bg-black flex flex-col justify-center items-center">
         <div className="text-gray-400 w-9/12 text-center m-2">
-          Click on your favorite category to find Netflix's hidden curated
+          Click on your favorite category to find Netflix&apos;s hidden curated
           categories
         </div>
         <LinkCards data={props.data} />
